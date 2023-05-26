4bcf145dde4aba1ba0738902bcc01406eced9693
Lame_on_flan <bandautuderov3@gmail.com>
2023-05-26 10:17:02 +0300

    Table

---
+++ Table-2.js
@@ -0,0 +1,12 @@
+var review = {
+    ID: '1',
+    author: 'John',
+    date: new Date('2023-05-26 15:30:00'),
+    comment: 'Отличный продукт!',
+    rating: {
+        service: 4,
+        price: 5,
+        value: 5,
+        quality: 4
+    }
+};
